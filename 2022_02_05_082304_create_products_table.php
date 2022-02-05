71691b3e2b7d4ff576730d2c75399dea2cf15cc7
rizalpeking21 <98934372+rizalpeking21@users.noreply.github.com>
2022-02-05 18:04:36 +0700

    Upload ulang pages dan upload produk

---
+++ 2022_02_05_082304_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string("product_name");
+            $table->string("product_price");
+            $table->string("product_desc");
+            $table->string("product_picture");
+            $table->string("product_category");
+            $table->string("user_id");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
