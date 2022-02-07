069361a7bf95f573205ef6db8971d46fb20c317c
randiprakoso <randi.prakoso1204@gmail.com>
2022-02-07 14:41:09 +0700

    Comment Beres

--- database/migrations/2022_02_07_073006_create_comment_table.php
+++ database/migrations/2022_02_07_073006_create_comment_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCommentsTable extends Migration
+class CreateCommentTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,8 @@ class CreateCommentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('comments', function (Blueprint $table) {
+        Schema::create('comment', function (Blueprint $table) {
             $table->id();
-            $table->string("user_id");
-            $table->string("product_id");
-            $table->string("comments_title");
-            $table->string("comments_content");
             $table->timestamps();
         });
     }
@@ -30,6 +26,6 @@ class CreateCommentsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('comments');
+        Schema::dropIfExists('comment');
     }
 }
